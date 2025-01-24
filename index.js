917a2e3d71554b0d6f13d740734d2419d1c76c5b
wintryice0 <148963506+wintryice0@users.noreply.github.com>
2025-01-24 18:39:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
 
-const extensionName = "wintryice0-wintry";
-const extensionFolderPath = `scripts/extensions/${extensionName}`;
-let isEnabled = false; // Keep track of whether asterisk removal is enabled
+const extensionName = "wintry";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {
+    enabled: false,
+};
 
-// Function to remove asterisks from a string
-function removeAsterisks(text) {
-    return text.replace(/\*/g, "");
-}
-
-// Function called for each chat message
-function processChatMessage(message) {
-    const context = getContext();
-    if (isEnabled && message.is_user === false) {
-        message.mes = removeAsterisks(message.mes);
-    }
-}
-
-// Function to toggle the extension on or off
-function toggleExtension() {
-    isEnabled = !isEnabled;
-    $("#wintry_toggle").val(isEnabled ? "Disable Asterisk Removal" : "Enable Asterisk Removal");
-    extension_settings[extensionName].enabled = isEnabled;
-    saveSettingsDebounced();
-    console.log(`Wintry extension toggled ${isEnabled ? "on" : "off"}`);
-}
-
-// Load settings
 async function loadSettings() {
     extension_settings[extensionName] = extension_settings[extensionName] || {};
     if (Object.keys(extension_settings[extensionName]).length === 0) {
-      Object.assign(extension_settings[extensionName], { enabled: false });
+        Object.assign(extension_settings[extensionName], defaultSettings);
     }
-    isEnabled = extension_settings[extensionName].enabled;
-    $("#wintry_toggle").val(isEnabled ? "Disable Asterisk Removal" : "Enable Asterisk Removal");
-    console.log("Wintry extension settings loaded.");
+
+    $("#wintry_enable").prop("checked", extension_settings[extensionName].enabled).trigger("input");
 }
 
-// Main function when the extension is loaded
+function onEnableInput(event) {
+    const enabled = Boolean($(event.target).prop("checked"));
+    extension_settings[extensionName].enabled = enabled;
+    saveSettingsDebounced();
+    toastr.success(`Wintry ${enabled ? "enabled" : "disabled"}`);
+}
+
 jQuery(async () => {
     const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
     $("#extensions_settings").append(settingsHtml);
-
-    $("#wintry_toggle").on("click", toggleExtension);
-
-    const context = getContext();
-    context.eventSource.on(context.eventCall, processChatMessage);
-
+    $("#wintry_enable").on("input", onEnableInput);
     loadSettings();
 });
